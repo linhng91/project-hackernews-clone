dddbbff205ba8b18d29e7f502e5de64b7c4e2915
Son Le Duc <jsonleduc@gmail.com>
2021-02-25 19:11:52 +0100

    Add Search bar without functionality

---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+
+export const Nav = () => {
+
+  return
+    <nav> TEST </nav>
+  );
+};
--- src/components/Story.js
+++ src/components/Story.js
@@ -2,7 +2,17 @@ import React, {useState, useEffect} from 'react';
 import { getStory } from '../services/hnApi'
 
 export const Story = ({storyId}) => {
-  useEffect(() => {}, []);
+  const [story, setStory] = useState({})
 
-  return <p>I am a story {storyId}!</p>;
+  useEffect(() => {
+    getStory(storyId).then(data => data && data.url && setStory(data));
+  }, []);
+
+  return story && story.url? (
+  <>
+  <a href= {story.url}><p>{story.title}</p></a>
+  By: <p>{story.by}</p>
+  Posted: <p>{story.time}</p>
+  </>
+) : null;
 }
